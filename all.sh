949c1440f016d533e79b2fe226dfc967c0123220
r.ishibashi <rysh.cact@gmail.com>
2019-10-25 05:49:15 +0900

    update sbt version

---
+++ all.sh
@@ -0,0 +1,49 @@
+cd .//scalacheck-example/
+sbt test
+
+cd ../
+cd .//sort-example/
+sbt test
+
+cd .//self-type-annotation-example/
+sbt test
+
+cd ../
+cd .//strict-scala/
+sbt test
+
+cd ../
+cd .//sbt-docker-example/
+sbt test
+
+cd ../
+cd .//better-files-example/
+sbt test
+
+cd ../
+cd .//forexp/
+sbt test
+
+cd ../
+cd .//treat-control-characters/
+sbt test
+
+cd ../
+cd .//s3-example/
+sbt test
+
+cd ../
+cd .//zio-example/
+sbt test
+
+cd ../
+cd .//scalajs-example/
+sbt test
+
+cd ../
+cd .//circe-example/
+sbt test
+
+cd ../
+cd .//cats-example/
+sbt test
